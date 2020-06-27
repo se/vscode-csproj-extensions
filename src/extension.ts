8ec75d408e9d784d4d115aaea98627e0443bf721
Selçuk Ermaya <selcukermaya@gmail.com>
2020-06-27 18:07:06 +0300

    Removed unnecessary usings.

--- src/extension.ts
+++ src/extension.ts
@@ -43,7 +43,7 @@ function getParameters(content: String) {
 export function activate(context: vscode.ExtensionContext) {
   console.log('"csproj-extensions" is activated and up and running!');
 
-  const pppp = vscode.languages.registerCompletionItemProvider("xml", {
+  const xmlCompletion = vscode.languages.registerCompletionItemProvider("xml", {
     provideCompletionItems(
       document: vscode.TextDocument,
       position: vscode.Position,
@@ -82,6 +82,7 @@ export function activate(context: vscode.ExtensionContext) {
         try {
           const file = fs.readFileSync(importFilePath, "utf-8");
           const parameters = getParameters(file);
+          console.debug(parameters);
 
           const completions: vscode.ProviderResult<
             | vscode.CompletionItem[]
@@ -103,23 +104,7 @@ export function activate(context: vscode.ExtensionContext) {
             completions.push(parameterCompletion);
           });
 
-          // a completion item that can be accepted by a commit character,
-          // the `commitCharacters`-property is set which means that the completion will
-          // be inserted and then the character will be typed.
-          const commitCharacterCompletion = new vscode.CompletionItem(
-            "console"
-          );
-          commitCharacterCompletion.commitCharacters = ["."];
-          commitCharacterCompletion.documentation = new vscode.MarkdownString(
-            "Press `.` to get `console.`"
-          );
-
-          completions.push(commitCharacterCompletion);
-          // a simple completion item which inserts `Hello World!`
-          // return all completion items as array
           return completions;
-
-          console.debug(parameters);
         } catch (error) {
           console.error(error);
         }
@@ -128,101 +113,18 @@ export function activate(context: vscode.ExtensionContext) {
     },
   });
 
-  const provider1 = vscode.languages.registerCompletionItemProvider(
-    "plaintext",
-    {
-      provideCompletionItems(
-        document: vscode.TextDocument,
-        position: vscode.Position,
-        token: vscode.CancellationToken,
-        context: vscode.CompletionContext
-      ) {
-        // a simple completion item which inserts `Hello World!`
-        const simpleCompletion = new vscode.CompletionItem("Hello World!");
+  context.subscriptions.push(xmlCompletion);
 
-        // a completion item that inserts its text as snippet,
-        // the `insertText`-property is a `SnippetString` which will be
-        // honored by the editor.
-        const snippetCompletion = new vscode.CompletionItem(
-          "Good part of the day"
-        );
-        snippetCompletion.insertText = new vscode.SnippetString(
-          "Good ${1|morning,afternoon,evening|}. It is ${1}, right?"
-        );
-        snippetCompletion.documentation = new vscode.MarkdownString(
-          "Inserts a snippet that lets you select the _appropriate_ part of the day for your greeting."
-        );
+  // let disposable = vscode.commands.registerCommand(
+  //   "csproj-extensions.toggleExtensionState",
+  //   () => {
+  //     // The code you place here will be executed every time your command is executed
+  //     // Display a message box to the user
+  //     vscode.window.showInformationMessage("Activated!");
+  //   }
+  // );
 
-        // a completion item that can be accepted by a commit character,
-        // the `commitCharacters`-property is set which means that the completion will
-        // be inserted and then the character will be typed.
-        const commitCharacterCompletion = new vscode.CompletionItem("console");
-        commitCharacterCompletion.commitCharacters = ["."];
-        commitCharacterCompletion.documentation = new vscode.MarkdownString(
-          "Press `.` to get `console.`"
-        );
-
-        // a completion item that retriggers IntelliSense when being accepted,
-        // the `command`-property is set which the editor will execute after
-        // completion has been inserted. Also, the `insertText` is set so that
-        // a space is inserted after `new`
-        const commandCompletion = new vscode.CompletionItem("new");
-        commandCompletion.kind = vscode.CompletionItemKind.Keyword;
-        commandCompletion.insertText = "new ";
-        commandCompletion.command = {
-          command: "editor.action.triggerSuggest",
-          title: "Re-trigger completions...",
-        };
-
-        // return all completion items as array
-        return [
-          simpleCompletion,
-          snippetCompletion,
-          commitCharacterCompletion,
-          commandCompletion,
-        ];
-      },
-    }
-  );
-
-  const provider2 = vscode.languages.registerCompletionItemProvider(
-    "plaintext",
-    {
-      provideCompletionItems(
-        document: vscode.TextDocument,
-        position: vscode.Position
-      ) {
-        // get all text until the `position` and check if it reads `console.`
-        // and if so then complete if `log`, `warn`, and `error`
-        const linePrefix = document
-          .lineAt(position)
-          .text.substr(0, position.character);
-        if (!linePrefix.endsWith("console.")) {
-          return undefined;
-        }
-
-        return [
-          new vscode.CompletionItem("log", vscode.CompletionItemKind.Method),
-          new vscode.CompletionItem("warn", vscode.CompletionItemKind.Method),
-          new vscode.CompletionItem("error", vscode.CompletionItemKind.Method),
-        ];
-      },
-    },
-    "." // triggered whenever a '.' is being typed
-  );
-
-  context.subscriptions.push(pppp, provider1, provider2);
-
-  let disposable = vscode.commands.registerCommand(
-    "csproj-extensions.toggleExtensionState",
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage("Activated!");
-    }
-  );
-
-  context.subscriptions.push(disposable);
+  // context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
